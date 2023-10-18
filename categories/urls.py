c9b39f73369edd90befdb34251369221b40e096e
shehab <shethr999@gmail.com>
2023-10-19 02:09:47 +0300

    create urls for all apps & folder for media in setting.py

--- categories/urls.py
+++ categories/urls.py
@@ -14,16 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path , include
-
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls')),
-    path('categories/',include('categories.urls')),
-    path('projects/',include('projects.urls')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+]
