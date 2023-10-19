a5a4db5393c47e8203888c92a70fe6d06465c911
sarahmohamed73 <sarahmoab73@gmail.com>
2023-10-20 00:50:09 +0200

    Slider Style

--- RaiseTogether/views.py
+++ RaiseTogether/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-highest_five_rated = [1,2,3,4,5]
 def index(request):
-  return render(request, 'homepage\index.html', context={"highest_five_rated":highest_five_rated})
+  return render(request, 'homepage\index.html')
