ebc2ac81990d4e86ba33662054e313ec7afbb09c
shehab <shethr999@gmail.com>
2023-10-19 07:56:08 +0300

    added superuser {iti:iti}

--- RaiseTogether/settings.py
+++ RaiseTogether/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'RaiseTogether.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django-project",
+        "USER": "django",
+        "PASSWORD": "7410",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
