0513ce679b4fbee24cd84239236da69120e9fc6e
sara-saraya <sarakamalsaraya@gmail.com>
2023-10-20 05:37:11 +0200

    fix, accounts model extends from abstractUser

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,19 +1,21 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from accounts.models import MyUser
 
 class Registeration(UserCreationForm):
     phone = forms.IntegerField(label='Phone name')
     image = forms.ImageField(label='Profile Picture')
 
     class Meta:
-        model = User
+        model = MyUser
         fields = ("first_name","last_name","email","username","password1","password2","phone","image")
+        # fields = '__all__'
 
     def clean_email(self):
             email = self.cleaned_data['email']
             if self.instance.email == email:
                 return email  
-            if User.objects.filter(email=email).exists():
+            if MyUser.objects.filter(email=email).exists():
                 raise forms.ValidationError("Email Already Exists.")
             return email
