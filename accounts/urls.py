ae35b99e4f9d411c915e4553afab4c407bd0b4f0
sara-saraya <sarakamalsaraya@gmail.com>
2023-10-20 07:07:00 +0200

    Revert "Merge pull request #3 from shehabth1999/accounts"
    This reverts commit 25ff36657a9f63da77417d38f2415d8aabb0bd63, reversing
    changes made to bb6e6df697b8a9bd8a0efb27d193588b6050203b.
    
    return

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path,include
-from accounts.views import Register, Profile
+from django.urls import path
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('register/', Register.as_view(), name = "register"),
-    path('profile/', Profile.as_view(), name = "profile"),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,3 @@
-from django.shortcuts import render,redirect,HttpResponse
-from django.urls import reverse, reverse_lazy
-from django.views.generic.edit import CreateView
-from django.views.generic import DetailView , DeleteView, UpdateView
-from django.contrib.auth.models import  User
-from accounts.forms import  Registeration
+from django.shortcuts import render
+
 # Create your views here.
-
-class Register(CreateView):
-    model = User
-    template_name = 'accounts/registeration.html'
-    form_class = Registeration
-    success_url = reverse_lazy('profile')
-
-class Profile(DetailView):
-    model = User
-    template_name = 'accounts/profile.html'
-    def get_object(self, queryset=None):
-        return self.request.user #return the currently logged-in user.
