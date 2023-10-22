0b3ce3cadaa22e7240193b3230324d538a1ea903
shehab <shethr999@gmail.com>
2023-10-23 00:06:25 +0300

    added in accounts (login, logout, edit, delete)

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path , include
-from accounts.views import Register, Profile
+from accounts.views import Register, Profile, Edit_Profile, Delete_Profile
 
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('register/', Register.as_view(), name = "register"),
-    path('profile/', Profile.as_view(), name = "profile"),
+    path('register/', Register.as_view(), name = "account.create"),
+    path('profile/', Profile.as_view(), name = "profile.view"),
+    path('edit/', Edit_Profile.as_view(), name = "profile.edit"),
+    path('delete/', Delete_Profile.as_view(), name = "profile.delete"),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -10,10 +10,28 @@ class Register(CreateView):
     model = MyUser
     template_name = 'accounts/registeration.html'
     form_class = Registeration
-    success_url = reverse_lazy('profile')
+    success_url = reverse_lazy('login')
 
 class Profile(DetailView):
     model = MyUser
     template_name = 'accounts/profile.html'
     def get_object(self, queryset=None):
         return self.request.user #return the currently logged-in user.
+
+class Edit_Profile(UpdateView):
+    model= MyUser
+    template_name = 'registration/edit_profile.html'
+    form_class = Registeration
+    success_url = reverse_lazy("login")
+    def get_object(self, queryset=None):
+        return self.request.user #return the currently logged-in user.
+
+ 
+
+class Delete_Profile(DeleteView):
+    model= MyUser
+    template_name = 'registration/delete_profile.html'
+    success_url = reverse_lazy("account.create")
+    def get_object(self, queryset=None):
+        return self.request.user #return the currently logged-in user.    
+    
