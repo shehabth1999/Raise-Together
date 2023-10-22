75ae6ef889babf3f8d24142462086bee112c43a7
sarahmohamed73 <sarahmoab73@gmail.com>
2023-10-22 21:19:15 +0200

    Add Category Model & Design Latest 5 Project

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from categories.models import Category
 # Register your models here.
+admin.site.register(Category)
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=100, unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+
+  def __str__(self):
+    return f"{self.name}"
+  
+  @classmethod
+  def get_all_categories(cls):
+    return  cls.objects.all()
